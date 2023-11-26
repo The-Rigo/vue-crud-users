e68a4645fd1e5d105805065c8e0ffa012e7989b6
Rodrigo Ariel Ledezma Lopez <rodri.ledezma9483@gmail.com>
2023-11-26 16:44:45 -0400

    add userCreateView, userEditView and UserView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import UserEditView from "@/views/users/UserEditView.vue";
+import UserView from "@/views/users/UserView.vue";
+import UserCreateView from "@/views/users/UserCreateView.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -16,6 +20,21 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/users',
+      name: 'user',
+      component: UserView
+    },
+    {
+      path: '/users/create',
+      name: 'userCreate',
+      component: UserCreateView
+    },
+    {
+      path: '/users/edit',
+      name: 'userEdit',
+      component: UserEditView
     }
   ]
 })
---
+++ src/views/users/UserEditView.vue
@@ -0,0 +1,14 @@
+
+<script>
+    import { defineComponent } from 'vue';
+    export default defineComponent ({
+        name:"UserEditView"
+    })
+</script>
+<template>
+    <h1>Form edit user</h1>
+</template>
+
+<style scoped>
+
+</style>
