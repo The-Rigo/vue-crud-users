3fdb22c09ba28f935aa13581404bcc74c7ca3b04
Rodrigo Ariel Ledezma Lopez <rodri.ledezma9483@gmail.com>
2023-11-27 11:38:50 -0400

    change UserView.vue with vuetify

--- src/views/users/UserView.vue
+++ src/views/users/UserView.vue
@@ -5,8 +5,10 @@ import axios from "axios";
 export default {
   name:"UserView",
   data(){
+
     return{
-      users:[]
+      users:[],
+      show: null
     }
   },
   methods:{
@@ -32,6 +34,11 @@ export default {
       const year = date.getFullYear();
 
       return `${day}-${month}-${year}`;
+    },
+    toggleDetail(userId) {
+      // Si el ID del usuario es igual al que ya está mostrando, ocultar el detalle
+      // De lo contrario, mostrar el detalle del usuario correspondiente
+      this.show = this.show === userId ? null : userId;
     }
   },
   mounted() {
@@ -40,60 +47,91 @@ export default {
 }
 </script>
 <template>
-    <div class="users">
-      <div class="card">
-        <div class="card-header">
-          <h4>
-            Users
-            <RouterLink to="/users/create" class="btn btn-primary float-end">
+  <!-- Acciones de la tarjeta, como botones, detallas, etc. -->
+  <div class="users">
+    <div class="card">
+      <div class="card-header">
+        <h4>
+          Users
+          <RouterLink to="/users/create" tag="v-btn">
+            <v-btn>
               Add User
-            </RouterLink>
-          </h4>
-        </div>
-        <div class="card-body">
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th>Nro.</th>
-                <th>Username</th>
-                <th>Password</th>
-                <th>Email</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Age</th>
-                <th>Birthday</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody v-if="users.length > 0">
-              <tr v-for="(user, index) in this.users" :key="index">
-                <td>{{ index + 1}}</td>
-                <td>{{ user.username }}</td>
-                <td>{{ user.password }}</td>
-                <td>{{ user.email }}</td>
-                <td>{{ user.first_name }}</td>
-                <td>{{ user.last_name }}</td>
-                <td>{{ user.age }}</td>
-                <td>{{ formatDate(user.birth_day) }}</td>
-                <td>
-                  <RouterLink :to="{ path: '/users/'+ user.id + '/edit' }" class="btn btn-success">
-                    Edit
-                  </RouterLink>
-                  <button type="button" @click="deleteUserById(user.id)" class="btn btn-danger" >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-            <tbody v-else>
-              <tr>
-                <td colspan="7">There are no users</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+            </v-btn>
+          </RouterLink>
+        </h4>
       </div>
+
+    <tbody v-if="users.length > 0">
+    <v-row>
+      <v-col
+          v-for="(user, index) in users"
+          :key="index"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3">
+      <v-card class="mx-auto my-8 "
+          max-width="344"
+          elevation="16" >
+        <v-card-item>
+          <v-card-title>
+            Username:  {{ user.username }}
+          </v-card-title>
+          <v-card-subtitle>
+            Email: {{ user.email }}
+          </v-card-subtitle>
+        </v-card-item>
+
+        <v-card-actions>
+          <RouterLink :to="{ path: '/users/'+ user.id + '/edit' }" tag="v-btn">
+            <v-btn color="teal-darken-4"
+                   variant="text">
+              Edit
+            </v-btn>
+          </RouterLink>
+          <button type="button" @click="deleteUserById(user.id)" >
+            <v-btn color="red-accent-4"
+                   variant="text">
+              Delete
+            </v-btn>
+          </button>
+          <v-spacer></v-spacer>
+          <v-btn
+              color="orange-lighten-2"
+              variant="text"
+              @click="toggleDetail(user.id)"
+          >Detail</v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+          <div v-show="show === user.id">
+            <v-divider></v-divider>
+
+            <v-card-text>
+              Password: {{ user.password }}
+              <v-divider></v-divider>
+              First Name: {{ user.first_name }}
+              <v-divider></v-divider>
+              Last Name: {{ user.last_name }}
+              <v-divider></v-divider>
+              Age: {{ user.age }}
+              <v-divider></v-divider>
+              Birthday: {{ formatDate(user.birth_day) }}
+
+            </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
+      </v-col>
+    </v-row>
+    </tbody>
+
+    <tbody v-else>
+    <tr>
+      <td colspan="7">There are no users</td>
+    </tr>
+    </tbody>
     </div>
+  </div>
 </template>
 
 <style scoped>
